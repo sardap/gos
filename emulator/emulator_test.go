ed48d5365123e75cb083f5cfe977376544eeda09
sardap <sardap@users.noreply.github.com>
2021-05-25 21:56:58 +1000

    added rest of undocumneted opcodes

--- emulator/emulator_test.go
+++ emulator/emulator_test.go
@@ -223,7 +223,7 @@ func TestNesTestRom(t *testing.T) {
 
 	cycles := int64(4)
 	lineNum := 1
-	for scanner.Scan() && lineNum < 5023 && !t.Failed() {
+	for scanner.Scan() && lineNum < 5151 && !t.Failed() {
 		line := scanner.Text()
 		nesTestLine := parseNesTestLine(string(line))
 		nesTestEmulator := emulatorToTestLine(e, cycles)

4e95773ccc17475ab0a0222232d319b6723324b6
sardap <sardap@users.noreply.github.com>
2021-05-26 19:02:39 +1000

    added window

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"log"
 	"os"
 
+	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/sardap/gos/emulator"
 )
+
+const (
+	WindowWidth  = 1200
+	WindowHeight = 720
+)
+
+type Gos struct {
+}
+
+func (g *Gos) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return WindowWidth, WindowHeight
+}
+
+func (g *Gos) Update() error {
+	return nil
+}
+
+func (g *Gos) Draw(screen *ebiten.Image) {
+}
 
 func main() {
 	e := emulator.Create()
 
 	func() {
-		f, err := os.Open("assets\\nestest\\nestest.nes")
+		f, err := os.Open("assets\\test_roms\\nestest.nes")
 		if err != nil {
 			panic(err)
 		}
@@ -20,7 +41,10 @@ func main() {
 		}
 	}()
 
-	for {
-		e.Step()
+	ebiten.SetWindowSize(WindowWidth, WindowHeight)
+	ebiten.SetWindowTitle("Go Boy")
+
+	if err := ebiten.RunGame(&Gos{}); err != nil {
+		log.Fatal(err)
 	}
 }
